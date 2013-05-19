c1f8919b17c1b7a03dfaa2fa4224d5969f4af649
maskerliu <lynx.chrisliu@gmail.com>
2013-05-19 13:00:57 +0800

    重写DataService协议定义，完成LCEnviroment类

--- Achilles/Achilles/core/LCDataService.h
+++ Achilles/Achilles/core/LCDataService.h
@@ -7,30 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LCDataServiceDelegate.h"
+
+@class LCTimestamp;
 
 
-
-
-
-typedef enum {
-    TRACK, // 单词行程,最小统计切片（default via min，当单次行程时间较长时适当合并行程）
-    WEEK, // 周,最小统计切片为日
-    MONTH, // 月,最小统计切片为日
-    YEAR // 按年,最小统计切片为月
-} Span; // 统计跨度
-
-
-@interface LCDataService : NSObject
+@interface LCDataService : NSObject <LCDataServiceDelegate>
 
 + (LCDataService *)sharedDataService;
 
-- (NSDictionary* )getActionsBy:(Span)span from:(long)beginTime to:(long)endTime;
+- (NSDictionary* )parseOriginData:(NSString* )originData;
 
-- (NSDictionary* )getOilCostBy:(Span)span from:(long)beginTime to:(long)endTime;
-
-- (NSDictionary* )getSpeedBy:(Span)span from:(long)beginTime to:(long)endTime;
-
-- (NSDictionary* )getTempBy:(Span)span from:(long)beginTime to:(long)endTime;
-
-- (void)getConfigWithSuccessBlock:(void (^)(NSDictionary *))success withFailBlock:(void (^)())fail;
 @end
--- Achilles/Achilles/core/LCDataService.m
+++ Achilles/Achilles/core/LCDataService.m
@@ -7,21 +7,27 @@
 //
 
 #import "LCDataService.h"
+#import "LCTimestamp.h"
 
 @implementation LCDataService
 
 static LCDataService *sharedDataService = nil;
 
-+ (LCDataService *)sharedDataService {
-	@synchronized(self) {
-		if (sharedDataService == nil) {
++ (LCDataService *)sharedDataService
+{
+	@synchronized(self)
+    {
+		if (sharedDataService == nil)
+        {
 			sharedDataService = [[LCDataService alloc] init];
 		}
 	}
 	return sharedDataService;
 }
 
-- (void)getConfigWithSuccessBlock:(void (^)(NSDictionary *))success withFailBlock:(void (^)())fail {
+- (void)getConfigWithSuccessBlock:(void (^)(NSDictionary *))success
+                    withFailBlock:(void (^)())fail
+{
 //	fail();
 	success(@{@"obd":@"xxxx", @"sid":@"xx11", @"indicator":@0});
 }
@@ -31,14 +37,10 @@ static LCDataService *sharedDataService = nil;
 // 2.每月、日平均/最佳油耗统计
 // 3.每月、日平均速度统计
 // 4.每月、日最佳温度统计
-- (NSDictionary* )getActionsBy:(Span)span from:(long)beginTime to:(long)endTime
-{
-    NSDate* beginDate = [NSDate dateWithTimeIntervalSince1970:beginTime];
-    NSDate* endDate = [NSDate dateWithTimeIntervalSince1970:endTime];
-    
-    switch (span) {
+- (NSDictionary* )getDrivingDataWithSpan:(LCTimestamp* )timestamp
+{    
+    switch ([timestamp span]) {
         case YEAR:
-            
             break;
         case MONTH:
             break;
@@ -51,63 +53,6 @@ static LCDataService *sharedDataService = nil;
     }
     return nil;
 }
-
-- (NSDictionary* )getOilCostBy:(Span)span from:(long)beginTime to:(long)endTime
-{
-    switch (span) {
-        case YEAR:
-            
-            break;
-        case MONTH:
-            break;
-        case WEEK:
-            break;
-        case TRACK:
-            break;
-        default:
-            break;
-    }
-    return nil;
-}
-
-- (NSDictionary* )getSpeedBy:(Span)span from:(long)beginTime to:(long)endTime
-{
-    switch (span) {
-        case YEAR:
-            
-            break;
-        case MONTH:
-            break;
-        case WEEK:
-            break;
-        case TRACK:
-            break;
-        default:
-            break;
-    }
-    return nil;
-}
-
-- (NSDictionary* )getTempBy:(Span)span from:(long)beginTime to:(long)endTime
-{
-    switch (span) {
-        case YEAR:
-            
-            break;
-        case MONTH:
-            break;
-        case WEEK:
-            break;
-        case TRACK:
-            break;
-        default:
-            break;
-    }
-    
-    return nil;
-}
-
-
 
 //
 // 解析原始数据
@@ -115,7 +60,7 @@ static LCDataService *sharedDataService = nil;
 // 1.单次行程summary数据
 // 2.单次行程分析后取得的各个时间片上的统计数据，主要包括各个时间点上的平均油耗、
 //
-- (NSDictionary* )parseOriginData:(NSString*) originData
+- (NSDictionary* )parseOriginData:(NSString* )originData
 {
     return nil;
 }
